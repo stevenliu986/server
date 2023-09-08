f2e6061f0acb7fd4c1596003bbeab3e5342a5de1
StevenLiu <stevenliu986@gmail.com>
2023-09-08 15:21:03 +1000

    add new lines

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import router from "./routes/user.routes";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/test");
-app.get("/", (req, res) => {
-  res.send("Hello TypeScript!");
-});
+app.use("/api", router);
 
 const port = process.env.PORT ?? 8080;
 
