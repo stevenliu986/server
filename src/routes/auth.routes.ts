f23d439cd161745d691f0a2070059dda37e63b98
StevenLiu <stevenliu986@gmail.com>
2023-09-10 11:32:09 +1000

    add new routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,3 +1,8 @@
 import express from "express";
+import { signOut, signIn } from "../controllers/auth.controller";
 
 const authRouter = express.Router();
+authRouter.get("auth/signout", signOut);
+authRouter.post("/auth/signin", signIn);
+
+export default authRouter;
