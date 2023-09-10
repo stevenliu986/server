1f11dbe0842dd3f0426a9e8e60fb5b5f8017db81
StevenLiu <stevenliu986@gmail.com>
2023-09-10 11:33:57 +1000

    add user authenticate functions

---
+++ src/utils/authenticate.ts
@@ -0,0 +1,9 @@
+import User from "../models/user.model";
+
+export const authenticate = (loginPassword: string) => {
+  return false;
+};
+
+export const authorize = (password: string) => {
+  return false;
+};
