8285dc6b599dd9366072efddb67269831bbc9408
StevenLiu <stevenliu986@gmail.com>
2023-09-10 22:26:31 +1000

    add signin/signout logic

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,37 +1,40 @@
 import { Request, Response } from "express";
 import User from "../models/user.model";
-// import jwt from "jsonwebtoken";
-// import expressJwt from "express-jwt";
+import jwt from "jsonwebtoken";
+import config from "../config/config"
+import expressJwt from "express-jwt";
 
-const signOut = async (req: Request, res: Response) => {
-  return null;
-};
 
 const signIn = async (req: Request, res: Response) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   try {
     let user = await User.findOne({ email }).exec();
     if (!user) {
-      return res.json("user not found");
+      return res.json("User not found");
     }
-    if (!user.authenticate("password")) {
-      console.log("Hello, I am the method that is defined in the model");
-    }
-    return res.json(user);
-    // if (!user) {
-    //   return res.status(401).json("User not found");
-    // }
-    // // TODO: 将匹配上的用户的密码与登录用户输入的密码比对
-    // if (!user.authenticate(password)) {
-    //   return res.status(401).json({ error: "email or password incorrect" });
-    // }
 
-    // const token = "111";
-    // res.cookie("token", token, { expires: new Date() });
-    // return res.json("Login successful!");
+    if (!user.authenticate(password)) {
+      return res.status(401).json({ error: "Email and password don't match." });
+    }
+
+    const token = jwt.sign({ _id: user._id }, config.jwtSecret);
+    res.cookie("t", token, { expires: new Date(), httpOnly: true });
+    return res.json({token, user: {
+      _id: user._id,
+      name:user.name,
+      email:user.email
+    }});
   } catch (error) {
-    return res.status(401).json({ error: error.message });
+    return res.status(401).json({ error: "Could not sign in." });
   }
 };
+
+const signOut = async (req: Request, res: Response) => {
+  res.clearCookie("t")
+  return res.status(200).json({message: "Sign out"});
+};
+
+const requireSignin = expressJwt({secret: config.jwtSecret,userProperty: "auth"})
+
 
 export { signOut, signIn };
