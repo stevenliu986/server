d63e39653397156e9515113f3a0ecbaaf88ba071
StevenLiu <stevenliu986@gmail.com>
2023-09-16 10:52:06 +1000

    update auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,8 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import User from "../models/user.model";
 import jwt from "jsonwebtoken";
-import config from "../config/config"
-import expressJwt from "express-jwt";
-
+import config from "../config/config";
+import { expressjwt, Request as JWTRequest } from "express-jwt";
 
 const signIn = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -19,22 +18,36 @@ const signIn = async (req: Request, res: Response) => {
 
     const token = jwt.sign({ _id: user._id }, config.jwtSecret);
     res.cookie("t", token, { expires: new Date(), httpOnly: true });
-    return res.json({token, user: {
-      _id: user._id,
-      name:user.name,
-      email:user.email
-    }});
+    return res.json({
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
   } catch (error) {
     return res.status(401).json({ error: "Could not sign in." });
   }
 };
 
 const signOut = async (req: Request, res: Response) => {
-  res.clearCookie("t")
-  return res.status(200).json({message: "Sign out"});
+  res.clearCookie("t");
+  return res.status(200).json({ message: "Sign out" });
 };
 
-const requireSignin = expressJwt({secret: config.jwtSecret,userProperty: "auth"})
+const requireSignIn = expressjwt({
+  secret: config.jwtSecret,
+  algorithms: ["HS256"],
+});
 
+// const hasAuthorization = (req: Request, res: Response, next: NextFunction) => {
+//   const authorized = req.profile && req.auth && req.profile._id == req.auth._id;
+//   if (!authorized) {
+//     return res.status(403).json({
+//       error: "User is not authorized!",
+//     });
+//   }
+// };
 
 export { signOut, signIn };
